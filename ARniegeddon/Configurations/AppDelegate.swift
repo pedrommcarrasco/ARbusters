629bb7b6231b1378ecc93464b89379dffea8948e
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-01-10 21:41:02 +0000

    Init repository

---
+++ ARniegeddon/Configurations/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ARniegeddon
+//
+//  Created by Pedro Carrasco on 10/01/18.
+//  Copyright © 2018 Pedro Carrasco. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
