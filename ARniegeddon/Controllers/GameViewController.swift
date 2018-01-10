bfbe48f98f906b5d007a96e4c515def448fddc81
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-01-10 22:11:17 +0000

    Implemented MainViewController
    Setups controller, loads scene, handles permission errors

--- ARniegeddon/Controllers/GameViewController.swift
+++ ARniegeddon/Controllers/GameViewController.swift
@@ -10,64 +10,50 @@ import ARKit
 
 class GameViewController: UIViewController, ARSCNViewDelegate {
 
+    // MARK: - OUTLETS
     @IBOutlet var sceneView: ARSKView!
-    
+
+    // MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
+        if let view = self.view as? ARSKView {
+            sceneView = view
+            sceneView.delegate = self
+
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .resizeFill
+            scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+            view.presentScene(scene)
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+}
+
+extension GameViewController: ARSKViewDelegate {
+    func sessionInterruptionEnded(_ session: ARSession) {
+        sceneView.session.run(session.configuration!,
+                              options: [.resetTracking,
+                                        .removeExistingAnchors])
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
     func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        let alert = UIAlertController(title: "Error",
+                                      message: "It seems we're having trouble launching ARniegeddon.",
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
